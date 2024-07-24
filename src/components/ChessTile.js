4dcc03ce41b3e2855aa1433834d94cf09cc8e36a
Olamikun <lammyaluko@gmail.com>
2024-07-24 15:16:47 -0600

    Used div with background instead of img element so that pieces can be draggable. Therefore, I used a hashmap to represent starting positions (no need for alt)

--- src/components/ChessTile.js
+++ src/components/ChessTile.js
@@ -1,30 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import "./ChessTile.css";
 
 /**
  * This function returns a color based on the position of the tile on the board
  * @param {String} file
- * @param {String} rank 
+ * @param {String} rank
  * @returns color
  */
 function getColor(file, rank) {
-    if ((file.charCodeAt(0) + parseInt(rank)) % 2 === 0) {
-        return '#4e7598';
-    }
-        return '#ebecd0';
+  if ((file.charCodeAt(0) + parseInt(rank)) % 2 === 0) {
+    return "#4e7598";
+  }
+  return "#ebecd0";
 }
 
-function ChessTile( {position, image} ) {
-    const file = position[0];
-    const rank = position[1];
-    const color = getColor(file, rank)
-    const [piece, setPiece] = useState(image);
+function ChessTile({ position, piece }) {
+  const file = position[0];
+  const rank = position[1];
+  const color = getColor(file, rank);
+  const [chessPiece, setChessPiece] = useState(piece);
 
-    return (
-        <div id='chesstile' style={{backgroundColor: color}}>
-            {image}
-        </div>
-    )
-};
+  return (
+    <div className="chesstile" style={{ backgroundColor: color }}>
+      {chessPiece ? ( // Tenary operator that only renders div if tile has a chess piece, improving performance.
+        <div
+          className="piece-div"
+          style={{ backgroundImage: `url('assets/images/${chessPiece}.png')` }}
+        ></div>
+      ) : null}
+    </div>
+  );
+}
 
 export default ChessTile;
