3d6cd54c947a59d5d172ac6cd53a6dfdd6b72170
Aidan <aidanhuang10@gmail.com>
2024-07-30 04:17:12 -0600

    Bishop Logic works just fine now

--- src/components/referee.js
+++ src/components/referee.js
@@ -77,19 +77,49 @@ export default class referee {
             }
         }
 
-        //Bishop Movement Logic 
+        // //Bishop Movement Logic 
+        // if(pieceType === 'bishop_w' || pieceType === 'bishop_b') {
+        //     //The difference in files MUST BE SAME as difference in ranks for a valid bishop move
+        //     if(Math.abs(currentFileNumber - previousFileNumber) === Math.abs(currentRank - previousRank)) {
+        //         // Forces it to compare using either the currentCoordinates or previousCoordinates depending
+        //         // on whether it is moving up or down diagonally
+        //         const minFile = Math.min(currentFileNumber, previousFileNumber);
+        //         const minRank = Math.min(currentRank, previousRank);
+        //         for (let i = 1; i < Math.abs(currentRank - previousRank); i++) {
+        //             console.log(this.numberToFile(minFile + i), minRank + i);
+        //             if(this.tileIsOccupied(this.numberToFile(minFile + i), minRank + i, boardState)) {
+        //                 console.log(boardState);
+        //                 return false;
+        //             }
+        //         }
+        //         // If it is occupied, then it must be either the same or opposing colour
+        //         if (!this.tileIsOccupied(currentFile, currentRank, boardState)) {
+        //             return true;
+        //         } else if (this.tileIsOccupiedByOpponent(currentFile, currentRank, boardState, teamColour)) {
+        //             return true;
+        //         }
+        //     }
+        // }
+
         if(pieceType === 'bishop_w' || pieceType === 'bishop_b') {
             //The difference in files MUST BE SAME as difference in ranks for a valid bishop move
             if(Math.abs(currentFileNumber - previousFileNumber) === Math.abs(currentRank - previousRank)) {
-                // Forces it to compare using either the currentCoordinates or previousCoordinates depending
-                // on whether it is moving up or down diagonally
-                const minFile = Math.min(currentFileNumber, previousFileNumber);
-                const minRank = Math.min(currentRank, previousRank);
+                // The two lines below handle whether or not the bishop is going up or down a path
+                let verticalDirection = (previousRank < currentRank) ? 1 : -1;
+                let counter = 0 + verticalDirection;
+                // Need to check whether or not bishop is going left or right
                 for (let i = 1; i < Math.abs(currentRank - previousRank); i++) {
-                    if(this.tileIsOccupied(this.numberToFile(minFile + i), minRank + i, boardState)) {
-                        // console.log(boardState);
-                        return false;
+                    if(previousFileNumber > currentFileNumber) { //Left
+                        if(this.tileIsOccupied(this.numberToFile(previousFileNumber - i), previousRank + counter, boardState)) {
+                            console.log(this.numberToFile(previousFileNumber - i), previousRank + counter)
+                            return false;
+                        }
+                    } else { //Right
+                        if(this.tileIsOccupied(this.numberToFile(previousFileNumber + i), previousRank + counter, boardState)) {
+                            return false;
+                        }
                     }
+                    counter += verticalDirection; // + or - one for each tile depending on vertical direction
                 }
                 // If it is occupied, then it must be either the same or opposing colour
                 if (!this.tileIsOccupied(currentFile, currentRank, boardState)) {
